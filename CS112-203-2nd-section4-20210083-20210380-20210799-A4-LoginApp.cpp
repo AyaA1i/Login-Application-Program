4b4e3a4b2d236de39b501346768ebba1dfd996d5
Aya Ali <101468593+ayaaalli@users.noreply.github.com>
2022-06-26 19:14:12 +0200

    Update and rename CS112-203-2nd-section4-20210083-20210380-20210799-A4-LoginApp.cpp to LoginApp.cpp

--- CS112-203-2nd-section4-20210083-20210380-20210799-A4-LoginApp.cpp
+++
@@ -1,5 +0,0 @@
-#include"CS112-203-2nd-section4-20210083-20210380-20210799-A4-LoginFunctions.h"
-int main() {
-    Show_Options();
-    return 0;
-}
---
+++ LoginApp.cpp
@@ -0,0 +1,5 @@
+#include"LoginFunctions.h"
+int main() {
+    Show_Options();
+    return 0;
+}
